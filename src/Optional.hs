7378c84042a318e39f78a406c38d66a494e72c9a
Hosshii <sao_heath6147.wistre@icloud.com>
2020-09-17 21:32:06 +0900

    optional

---
+++ src/Optional.hs
@@ -0,0 +1,16 @@
+import Data.Char
+
+-- findKey :: (Eq k) => k -> [(k, v)] -> v
+-- findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+
+-- findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+-- findKey key [] = Nothing
+-- findKey key ((k, v) : xs)
+--   | key == k = Just v
+--   | otherwise = findKey key xs
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
+
+string2digits :: String -> [Int]
+string2digits = map digitToInt . filter isDigit
