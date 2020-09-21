86c6b98ae7c4369b14b9d3f92e7b6e1262d9b00d
Hosshii <sao_heath6147.wistre@icloud.com>
2020-09-21 23:46:16 +0900

    data

---
+++ src/Type.hs
@@ -0,0 +1,12 @@
+-- data Shape
+--   = Circle Float Float Float
+--   | Rectangle Float Float Float Float
+--   deriving (Show)
+
+-- area :: Shape -> Float
+-- area (Circle _ _ r) = pi * r ^ 2
+-- area (Rectangle x1 x2 y1 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+data Point = Point Float Float deriving (Show)
+
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
